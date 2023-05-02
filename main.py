8baf78dc44100133595ef40313422923bc8e4d33
schmiedr <schmied.romeo05@gmail.com>
2023-05-02 11:54:12 +0200

    Schmied Rómeó Márk. Ki számoltam, hogy mennyi a gyémánt karátja.

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import pandas as pd
 
 if __name__ == '__main__':
-    diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
-    ##print(diamonds)
+    gyemantok = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
+    # gyémántok adatokat letöltjük
 
     ##print(diamonds['carat'])
+    # csak az oszlop megjelenítése
 
+
+    #Minta az összegzésre
     sum = 0
-    for i in diamonds['carat']:
+    for i in gyemantok['carat']:
         sum += i
     print('karát összesen: ' + str(sum))
 
-    darabszam = diamonds['carat'].count()
-    print('darabszám: '+ str(darabszam))
 
-
-    atlag = sum / darabszam
+    atlag = sum / gyemantok.count()
     print('átlag: ' + str(atlag))
+
+
+
+    szum = 0
+
+
+    for gyemant in gyemantok.iterrows():
+        pass
+        # ha a gyémánt színe 'H'
+        # akkor addjuk össze a price oszolopból az értékeket
+
+    for gyemant in gyemantok.iterrows():
+        color = gyemant[1]['color']
+        price = gyemant[1]['price']
+        if color == 'H':
+            szum += price
+            # print(gyemant)
+
+    print("Összes ára: " + str(szum))
